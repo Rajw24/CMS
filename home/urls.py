0501c2b6d4d0164c6a5ec2bff20105d08f701bcc
Raj Walavalkar <rwalavalkar724@gmail.com>
2023-02-22 01:50:59 +0530

    Login system added

--- home/urls.py
+++ home/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('signin/', views.signin, name="signin"),
     path('signup/', views.signup, name="signup"),
     path('signout/', views.signout, name="signout"),
+    path('activate/<uidb64>/<token>', views.activate, name="activate"),
 ]
--- home/views.py
+++ home/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMessage
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from home.tokens import generate_token
 from CMS import info
 
 # Create your views here.
@@ -22,11 +27,12 @@ def signin(request):
         if user is not None:
             login(request, user)
             messages.success(request, "Logged in successfully.")
-            return render(request, "home.html")
+            return render(request, "home.html") 
         else:
             messages.error(request, "Bad credentials")
             return redirect("home")
     return render(request, "home.html")
+    
 def signup(request):
     if request.method == "POST":
         
@@ -60,16 +66,52 @@ def signup(request):
         user.is_active = False
         user.save()
 
+        #Welcome Email
         subject = "Welcome to CMS"
-        message = f"Hello {user.first_name} !!\nWelcome to CMS. Thank you for Signing up \nWe have also sent you a conformation email, please confirm your email address in order to activate your account." 
+        message = f"Hello {user.first_name} {user.last_name} !!\nWelcome to CMS. Thank you for Signing up \nWe have also sent you a conformation email, please confirm your email address in order to activate your account." 
         from_email = info.EMAIL_HOST_USER
         to_email = [user.email]
         send_mail(subject, message, from_email, to_email, fail_silently=False)
 
-        messages.success(request, "You have successfully signed up. please login!")
+        #Confirmation email
+        current_site = get_current_site(request)
+        email_subject = "Email Verification at CMS"
+        email_message = render_to_string("email.html", {
+            'name' : user.first_name,
+            'domain' : current_site.domain,
+            'uid' : urlsafe_base64_encode(force_bytes(user.pk)),
+            'token': generate_token.make_token(user)
+        })
+        Confirmation_email = EmailMessage(
+            email_subject,
+            email_message,
+            from_email,
+            to_email,
+        ) 
+
+        Confirmation_email.fail_silently = True
+        Confirmation_email.send()
+
+        messages.success(request, "You have successfully signed up. please check your E-mail to login!")
         return redirect('home')
     return render(request, 'home.html')
+
 def signout(request):
     logout(request)
     messages.success(request, "Loged out Successfully.")
     return redirect('home')
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and generate_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return redirect('home') 
+    else:
+        return render(request, 'Activation_failed.html')
